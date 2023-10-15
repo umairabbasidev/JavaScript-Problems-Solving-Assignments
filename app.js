fe082f06bb76748ddae2f205962c7490f514a8c5
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-10-15 00:05:16 -0700

    problem solving questions of assignment one completd

---
+++ app.js
@@ -0,0 +1,148 @@
+// Solutions  Of Chapter 1  Questions  
+
+
+// Question 1
+
+// alert("Greeting")
+
+// Question 2
+
+// alert("Erro! Please entera valid passwird")
+
+// Question 3 
+
+// alert("Welcome to JS Land \nHappyCodind")
+
+// Question 4
+
+// let message = "Welcome to JS Land"
+// alert(message)
+// message = "Happy Coding!"
+// alert(message)
+
+// Question 5
+
+// console.log("Hello... I can run JS through my web browser's console");
+
+
+// ___________X___________X_____________Chapter 1 answers completed
+
+// Solutions  Of Chapter 2  Questions  
+
+
+// Question 1
+// let userName;
+
+// Question 2
+// let myName = "Muhammad Umair Abbasi";
+
+// Question 3
+// (a)
+// let message;
+// // (b)
+// message = "Hello World"
+// // (c)
+// alert(message)
+
+// Question 4
+// let studentName = "Jhone Doe"
+// let studentAge = "15 years old"
+// let certifiedCourse = "Web And App Development"
+// alert(studentName)
+// alert(studentAge)
+// alert(certifiedCourse)
+
+// question 5
+// let food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(food)
+
+
+// Question 6
+
+// let email = "example@gmail.com"
+// alert("My email address is " + email)
+
+
+
+
+// Question 7
+// let book = "\"A \nsmarter way to learn JavaScript\""
+// alert(book)
+
+// Question 8
+
+// let content = " <h1>Yeah! I can write HTML content through JS</h1>"
+// document.write(content)
+
+
+// question 9
+// let str = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(str)
+
+
+
+// __________X___________X__________chapter 2 answers completed
+
+
+// Solutions  Of Chapter 3  Questions  
+
+
+// Question 1
+
+// let age = 21;
+// alert("I am  " + age + " years old")
+
+// Question 2
+
+// let tracking  = 14
+// alert("You have visited this sites " + tracking + " times")
+
+
+// Question 3
+
+// let birthYear = 2002;
+// document.write("My birthday year is " +  birthYear   + " <br/>Data type of  my declared variable is number");
+
+
+
+
+// __________X___________X__________chapter 3 answers completed
+
+
+// Solutions  Of Chapter 4  Questions
+
+// Question 1
+
+// let var1 = var2 = var3 = 1
+// console.log(var3)
+
+// Question 2
+// legal variable name
+
+// let myName;
+// let name;
+// let my_name;
+// let $my;
+// let _underscore;
+
+// ilegal variable name
+
+
+// let 1st;
+// let my-name;
+// let 123;
+// let var;
+// let casesensitivE;
+
+// Question 3
+
+
+// document.write("<h1>Rules for naming JS variables </h1>" + "Variable names can only contain,numbers,$ and _.For example: $my_1stVariable" +'<br></br>' + "Variables must begin with a letter, $ or _. For example $name,-name or name" +'<br></br>' + "Variable names are case sensitive" +'<br></br>' +   "Variable names should not be JS keyword")
+
+
+
+
+
+
+
+
