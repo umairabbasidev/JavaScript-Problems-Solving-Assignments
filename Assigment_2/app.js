f8b5f0a6e3cc00b44b48f39c7e67cfc30fd229f7
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-10-20 05:40:03 -0700

    chapter 5 completed

---
+++ Assigment_2/app.js
@@ -0,0 +1,76 @@
+// Solutions  Of Chapter 5 Questions  
+
+// Question 1 
+
+// let num1 = 3;
+// let num2 = 5;
+// let addNum = num1 + num2;
+// document.write("Sum of 3 + 5 is:",addNum);
+
+// Question 2
+
+// let num1 = 3;
+// let num2 = 5;
+// let subtractNum = num2 - num1;
+// document.write("Subtractions of 5 - 3 is: ", subtractNum);
+
+// let num1 = 3;
+// let num2 = 5;
+// let multiplayNum = num2 * num1;
+// document.write("Multiplay of 5 * 3 is: ", multiplayNum);
+
+// let num1 = 3;
+// let num2 = 5;
+// let DivideNum = num2 / num1;
+// document.write("Division  of 5 - 3 is: ", DivideNum.toFixed(2));
+
+
+// Question 3
+
+let myVariable;
+// document.write(`Value
+// after variable declaration is:`)
+
+myVariable = 5
+
+// document.write(`Initial
+// value:`,myVariable)
+
+myVariable++;
+
+// document.write(`Value
+// after increment is:`,myVariable)
+
+myVariable = myVariable + 7;
+
+// document.write(`Value after addition is:`,myVariable)
+
+--myVariable
+ // document.write(`Value
+ // after decrement is:`,myVariable)
+ 
+
+ let finalVariableIs = myVariable % 3;
+
+ // document.write(`The remainder is:`,finalVariableIs)
+
+
+ // Question 4
+
+
+ // let ticketPrice = 600
+ // let numberOfTicket = 5;
+ 
+ // document.write(`Total cost to buy ${numberOfTicket} tickets to a movie is:${ticketPrice*numberOfTicket}PKR`)
+
+
+
+  // Question 5
+
+  for (let i = 1; i <= 10; i++) {
+
+   document.write(" 2 " + " x " + [i] + " = " + 2*[i] + "<br>") 
+
+  }
+
+
--- Assignment_1/app.js
+++ Assignment_1/app.js
@@ -139,7 +139,7 @@
 
 // document.write("<h1>Rules for naming JS variables </h1>" + "Variable names can only contain,numbers,$ and _.For example: $my_1stVariable" +'<br></br>' + "Variables must begin with a letter, $ or _. For example $name,-name or name" +'<br></br>' + "Variable names are case sensitive" +'<br></br>' +   "Variable names should not be JS keyword")
 
-
+// _________X__________X__________________ Assignemnt one completd
 
 
 
